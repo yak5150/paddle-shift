851b17d13cc4027dfc923999653f2bd755d4148a
Jakob Werle <jakob.werle@temple.edu>
2024-04-10 23:14:06 -0400

    shiftCount can count shifts per lap

--- fsae_michigan18_&_madformulateam_mft02_evo_&_Yak_5150_&_stint_3/shiftCount.py
+++ fsae_michigan18_&_madformulateam_mft02_evo_&_Yak_5150_&_stint_3/shiftCount.py
@@ -2,12 +2,19 @@ import pandas as pd
 
 data = pd.read_csv('fsae_michigan18_&_madformulateam_mft02_evo_&_Yak_5150_&_stint_3.csv')
 
-grouped_data = data.groupby('Lap')
+lap_group = data.groupby('Lap')
 
 shifts_per_lap = {}
 
-for lap, lap_data in grouped_data:
-    shifts = sum(1 for i in range(1, len(lap_data)) if lap_data['Gear'].iloc[i] != lap_data['Gear'].iloc[i-1])
+for lap, lap_data in lap_group:
+
+    gear_column = lap_data.iloc[:,5]
+
+    shifts = 0
+    for i in range(1, len(lap_data)):
+        if gear_column.iloc[i] != 0 and gear_column.iloc[i] != gear_column.iloc[i-1]:
+            shifts += 1
+
     shifts_per_lap[lap] = shifts
 
 for lap, shifts in shifts_per_lap.items():
