c8cdcfc2b7e8a86216e61b324f5b0f3ffc60ed95
Jakob Werle <jakob.werle@temple.edu>
2024-04-11 01:56:37 -0400

    shiftCount converting to arduino format

--- sim_shift_count/shiftCount.py
+++ sim_shift_count/shiftCount.py
@@ -2,37 +2,44 @@ import pandas as pd
 
 data = pd.read_csv('fsae_michigan18_&_madformulateam_mft02_evo_&_Yak_5150_&_stint_3.csv')
 data = data[data['Lap'] > 0]
+
 lap_group = data.groupby('Lap')
 
-shifts_per_lap = {}
+shifts = []
+times = []
 
 for lap, lap_data in lap_group:
     gear_column = lap_data['Gear']
+    time_column = lap_data['Time']
+    lap_shifts = []
+    lap_times = []
 
-    up_shifts = 0
-    down_shifts = 0
     last_non_zero_gear = None
+    last_shift_time = 0.0
+
     for gear in gear_column:
         if gear != 0:
             if last_non_zero_gear is not None:
                 if gear > last_non_zero_gear:
-                    up_shifts += 1
+                    lap_shifts.append(1)
                 elif gear < last_non_zero_gear:
-                    down_shifts += 1
+                    lap_shifts.append(0)
+                lap_times.append(last_shift_time)
+                last_shift_time = 0.0
             last_non_zero_gear = gear
+        else:
+            last_shift_time += 0.05 #sampling frequency
 
-    shifts_per_lap[lap] = {'up_shifts': up_shifts, 'down_shifts': down_shifts}
+    shifts.extend(lap_shifts)
+    times.extend(lap_times)
 
+Standard_Shifts = len(shifts)
+Standard_Times = len(times)
 
-for lap, shifts in shifts_per_lap.items():
-    print(f"Lap {lap}: {shifts['up_shifts']} upshifts - {shifts['down_shifts']} downshifts")
+Standard_Shift = ','.join(str(shift) for shift in shifts)
+Standard_Time = ','.join(str(time) for time in times)
 
-total_up_shifts = sum(shifts['up_shifts'] for shifts in shifts_per_lap.values())
-total_down_shifts = sum(shifts['down_shifts'] for shifts in shifts_per_lap.values())
-total_stint_shifts = total_up_shifts + total_down_shifts
-total_endurance_shifts = total_stint_shifts * 2
-
-print("Total upshifts counted:", total_up_shifts)
-print("Total downshifts counted:", total_down_shifts)
-print("Total stint shifts:", total_stint_shifts)
-print("Total endurance shifts:", total_endurance_shifts)
+print("#define Standard_Shifts", Standard_Shifts)
+print("#define Standard_Times", Standard_Times)
+print("const int Standard_Shift[] = {", Standard_Shift, "};")
+print("const float Standard_Time[] = {", Standard_Time, "};")
