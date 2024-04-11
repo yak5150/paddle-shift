5d5473ffa831c3f7a1254b0996f35b2630e1981f
Jakob Werle <jakob.werle@temple.edu>
2024-04-11 02:05:51 -0400

    working to fix shift time

--- sim_shift_count/shiftCount.py
+++ sim_shift_count/shiftCount.py
@@ -10,25 +10,26 @@ times = []
 
 for lap, lap_data in lap_group:
     gear_column = lap_data['Gear']
-    time_column = lap_data['Time']
+    time_column = lap_data['Time [s]']
     lap_shifts = []
     lap_times = []
 
     last_non_zero_gear = None
     last_shift_time = 0.0
 
-    for gear in gear_column:
+    for i in range(len(gear_column)):
+        gear = gear_column.iloc[i]
+        time = time_column.iloc[i]
+
         if gear != 0:
             if last_non_zero_gear is not None:
                 if gear > last_non_zero_gear:
-                    lap_shifts.append(1)
+                    lap_shifts.append(1)  # Upshift
                 elif gear < last_non_zero_gear:
-                    lap_shifts.append(0)
-                lap_times.append(last_shift_time)
-                last_shift_time = 0.0
+                    lap_shifts.append(0)  # Downshift
+                lap_times.append(time - last_shift_time)
+                last_shift_time = time
             last_non_zero_gear = gear
-        else:
-            last_shift_time += 0.05 #sampling frequency
 
     shifts.extend(lap_shifts)
     times.extend(lap_times)
@@ -37,7 +38,7 @@ Standard_Shifts = len(shifts)
 Standard_Times = len(times)
 
 Standard_Shift = ','.join(str(shift) for shift in shifts)
-Standard_Time = ','.join(str(time) for time in times)
+Standard_Time = ','.join(str(round(time, 2)) for time in times)
 
 print("#define Standard_Shifts", Standard_Shifts)
 print("#define Standard_Times", Standard_Times)
