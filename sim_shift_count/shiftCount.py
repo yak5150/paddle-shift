59c2edb26fc3e7ebaa6a12a37e75e1ec381130f6
Jakob Werle <jakob.werle@temple.edu>
2024-04-12 00:33:54 -0400

    shift counter works but still need to manually initialize time

--- sim_shift_count/shiftCount.py
+++ sim_shift_count/shiftCount.py
@@ -3,38 +3,30 @@ import pandas as pd
 data = pd.read_csv('fsae_michigan18_&_madformulateam_mft02_evo_&_Yak_5150_&_stint_3.csv')
 data = data[data['Lap'] > 0]
 
-lap_group = data.groupby('Lap')
+gear_column = data['Gear']
+time_column = data['Time [s]']
 
 shifts = []
 times = []
 
-for lap, lap_data in lap_group:
-    gear_column = lap_data['Gear']
-    time_column = lap_data['Time [s]']
-    lap_shifts = []
-    lap_times = []
+last_non_zero_gear = None
+last_shift_time = 75.35
 
-    last_non_zero_gear = None
-    last_shift_time = 0.0
+for i in range(len(gear_column)):
+    gear = gear_column.iloc[i]
+    time = time_column.iloc[i]
 
-    for i in range(len(gear_column)):
-        gear = gear_column.iloc[i]
-        time = time_column.iloc[i]
-
-        if gear != 0:
-            print(f"Gear: {gear}, Time: {time}, Last Gear: {last_non_zero_gear}, Last Shift Time: {last_shift_time}")
-            if last_non_zero_gear is not None:
-                if gear > last_non_zero_gear:
-                    lap_shifts.append(1)  # Upshift
-                elif gear < last_non_zero_gear:
-                    lap_shifts.append(0)  # Downshift
-                if gear != last_non_zero_gear:
-                    lap_times.append(time - last_shift_time)
-                    last_shift_time = time
-            last_non_zero_gear = gear
-
-    shifts.extend(lap_shifts)
-    times.extend(lap_times)
+    if gear != 0:
+        if last_non_zero_gear is not None:
+            if gear > last_non_zero_gear:
+                shifts.append(1)  # Upshift
+            elif gear < last_non_zero_gear:
+                shifts.append(0)  # Downshift
+            if gear != last_non_zero_gear:
+                # print(f"Gear: {gear}, Time: {time}, Last Gear: {last_non_zero_gear}, Last Shift Time: {last_shift_time}")
+                times.append(time - last_shift_time)
+                last_shift_time = time
+        last_non_zero_gear = gear
 
 Standard_Shifts = len(shifts)
 Standard_Times = len(times)
